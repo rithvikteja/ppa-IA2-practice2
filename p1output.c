7360b0f6639f153501feb8cd973c8de779a81b4d
Gayathri K <95470777+rithvikteja@users.noreply.github.com>
2022-03-06 08:41:23 +0000

    gayathri

---
+++ p1output.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void input(float *base, float *height)
+{
+  printf("enter b,h values\n");
+  scanf("%f%f",base,height);
+}
+void find_area(float base,float height,float *area)
+{
+  *area=base*height/2.0;
+}
+void output(float base,float height,float area)
+{
+  printf("the area of triangle=%f",area);
+}
+int main()
+{
+  float b,h,area;
+  input(&b,&h);
+  find_area(b,h,&area);
+  output(b,h,area);
+  }
---
+++ p2output.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int input_side()
+{
+  int side;
+  printf("enter the side of a triangle\n");
+  scanf("%d",&side);
+  return side;
+}
+int check_scalene(int a,int b,int c)
+{
+  if(a!=b && a!=c && b!=c)
+  {
+    return 1;
+  }
+  return 0;
+}
+void output(int a,int b,int c,int isscalene)
+{
+  if (isscalene)
+    {
+    printf("the traingle with sides %d,%d & %d is scalene\n",a,b,c);
+ }
+else
+  {
+  printf("the triangle with sides %d,%d & %d is not scalene\n",a,b,c);
+}
+  }
+int main()
+{
+  int AB=input_side();
+  int BC=input_side();
+  int CA=input_side();
+  int res=check_scalene(AB,BC,CA);
+  output(AB,BC,CA,res);
+  return 0;
+}
